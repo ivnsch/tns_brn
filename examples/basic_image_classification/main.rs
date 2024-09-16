e73801c98c874b5dfead2d286795991eb65cc53e
Ivan Schuetz <ivanschuetz@gmail.com>
2024-09-16 16:46:25 +0200

    get commented code to compile..

--- examples/basic_image_classification/main.rs
+++ examples/basic_image_classification/main.rs
@@ -9,7 +9,10 @@ mod plot;
 mod training;
 
 use burn::{
-    backend::{Autodiff, Wgpu},
+    backend::{
+        wgpu::{JitBackend, WgpuRuntime},
+        Autodiff, Wgpu,
+    },
     config::Config,
     data::{
         dataloader::DataLoaderBuilder,
@@ -82,8 +85,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let device = burn::backend::wgpu::WgpuDevice::default();
     let config = TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new());
 
-    // let batcher_train = MnistBatcher::<MyAutodiffBackend>::new(device.clone());
-    // let batcher_valid = MnistBatcher::<MyAutodiffBackend>::new(device.clone());
+    let batcher_train = MnistBatcher::<MyAutodiffBackend>::new(device.clone());
+    let batcher_valid =
+        MnistBatcher::<<MyAutodiffBackend as AutodiffBackend>::InnerBackend>::new(device.clone());
+    // let batcher_valid = MnistBatcher::<<Autodiff<burn_fusion::backend::Fusion<JitBackend<WgpuRuntime, f32, i32>>> as AutodiffBackend>::InnerBackend>::new(device.clone());
 
     let data_set = MnistDataset::train();
     let first = data_set.get(0);
@@ -91,40 +96,40 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     plot(first.unwrap())?;
 
-    train::<MyAutodiffBackend>(&artifact_dir, config, device);
+    // train::<MyAutodiffBackend>(&artifact_dir, config, device);
 
-    // let dataloader_train = DataLoaderBuilder::new(batcher_train)
-    //     .batch_size(config.batch_size)
-    //     .shuffle(config.seed)
-    //     .num_workers(config.num_workers)
-    //     .build(MnistDataset::train());
+    let dataloader_train = DataLoaderBuilder::new(batcher_train)
+        .batch_size(config.batch_size)
+        .shuffle(config.seed)
+        .num_workers(config.num_workers)
+        .build(MnistDataset::train());
 
-    // let dataloader_test = DataLoaderBuilder::new(batcher_valid)
-    //     .batch_size(config.batch_size)
-    //     .shuffle(config.seed)
-    //     .num_workers(config.num_workers)
-    //     .build(MnistDataset::test());
+    let dataloader_test = DataLoaderBuilder::new(batcher_valid)
+        .batch_size(config.batch_size)
+        .shuffle(config.seed)
+        .num_workers(config.num_workers)
+        .build(MnistDataset::test());
 
-    // let learner = LearnerBuilder::new(artifact_dir)
-    //     .metric_train_numeric(AccuracyMetric::new())
-    //     .metric_valid_numeric(AccuracyMetric::new())
-    //     .metric_train_numeric(LossMetric::new())
-    //     .metric_valid_numeric(LossMetric::new())
-    //     .with_file_checkpointer(CompactRecorder::new())
-    //     .devices(vec![device.clone()])
-    //     .num_epochs(config.num_epochs)
-    //     .summary()
-    //     .build(
-    //         config.model.init::<MyAutodiffBackend>(&device),
-    //         config.optimizer.init(),
-    //         config.learning_rate,
-    //     );
+    let learner = LearnerBuilder::new(artifact_dir)
+        .metric_train_numeric(AccuracyMetric::new())
+        .metric_valid_numeric(AccuracyMetric::new())
+        .metric_train_numeric(LossMetric::new())
+        .metric_valid_numeric(LossMetric::new())
+        .with_file_checkpointer(CompactRecorder::new())
+        .devices(vec![device.clone()])
+        .num_epochs(config.num_epochs)
+        .summary()
+        .build(
+            config.model.init::<MyAutodiffBackend>(&device),
+            config.optimizer.init(),
+            config.learning_rate,
+        );
 
-    // let model_trained = learner.fit(dataloader_train, dataloader_test);
+    let model_trained = learner.fit(dataloader_train, dataloader_test);
 
-    // model_trained
-    //     .save_file(format!("{artifact_dir}/model"), &CompactRecorder::new())
-    //     .expect("Trained model should be saved successfully");
+    model_trained
+        .save_file(format!("{artifact_dir}/model"), &CompactRecorder::new())
+        .expect("Trained model should be saved successfully");
 
     Ok(())
 }

c19686cd43a7de5b2ebfc6c6ca656965a670a192
Ivan Schuetz <ivanschuetz@gmail.com>
2024-09-17 17:30:40 +0200

    simplify model..

--- examples/basic_image_classification/model.rs
+++ examples/basic_image_classification/model.rs
@@ -1,18 +1,10 @@
 use burn::{
-    nn::{
-        conv::{Conv2d, Conv2dConfig},
-        pool::{AdaptiveAvgPool2d, AdaptiveAvgPool2dConfig},
-        Dropout, DropoutConfig, Linear, LinearConfig, Relu,
-    },
+    nn::{Linear, LinearConfig, Relu},
     prelude::*,
 };
 
 #[derive(Module, Debug)]
 pub struct Model<B: Backend> {
-    conv1: Conv2d<B>,
-    conv2: Conv2d<B>,
-    pool: AdaptiveAvgPool2d,
-    dropout: Dropout,
     linear1: Linear<B>,
     linear2: Linear<B>,
     activation: Relu,
@@ -30,13 +22,9 @@ impl ModelConfig {
     /// Returns the initialized model.
     pub fn init<B: Backend>(&self, device: &B::Device) -> Model<B> {
         Model {
-            conv1: Conv2dConfig::new([1, 8], [3, 3]).init(device),
-            conv2: Conv2dConfig::new([8, 16], [3, 3]).init(device),
-            pool: AdaptiveAvgPool2dConfig::new([8, 8]).init(),
-            activation: Relu::new(),
-            linear1: LinearConfig::new(16 * 8 * 8, self.hidden_size).init(device),
+            linear1: LinearConfig::new(28 * 28, self.hidden_size).init(device),
             linear2: LinearConfig::new(self.hidden_size, self.num_classes).init(device),
-            dropout: DropoutConfig::new(self.dropout).init(),
+            activation: Relu::new(),
         }
     }
 }
@@ -48,21 +36,18 @@ impl<B: Backend> Model<B> {
     pub fn forward(&self, images: Tensor<B, 3>) -> Tensor<B, 2> {
         let [batch_size, height, width] = images.dims();
 
+        // println!(
+        //     "in forward, batch_size: {} height: {} width: {} ",
+        //     batch_size, height, width
+        // ); // batch_size: 64 height: 28 width: 28
+
         // Create a channel.
         let x = images.reshape([batch_size, 1, height, width]);
 
-        let x = self.conv1.forward(x); // [batch_size, 8, _, _]
-        let x = self.dropout.forward(x);
-        let x = self.conv2.forward(x); // [batch_size, 16, _, _]
-        let x = self.dropout.forward(x);
-        let x = self.activation.forward(x);
-
-        let x = self.pool.forward(x); // [batch_size, 16, 8, 8]
-        let x = x.reshape([batch_size, 16 * 8 * 8]);
+        // collapse 3 dim into 1 (so we've 64 batch size and 784 entries per batch)
+        let x = x.reshape([batch_size, 1 * 28 * 28]); // 64, 784 (784 is 1 * 28 * 28)
         let x = self.linear1.forward(x);
-        let x = self.dropout.forward(x);
-        let x = self.activation.forward(x);
-
-        self.linear2.forward(x) // [batch_size, num_classes]
+        let x = self.linear2.forward(x); // [batch_size, num_classes]
+        self.activation.forward(x)
     }
 }
